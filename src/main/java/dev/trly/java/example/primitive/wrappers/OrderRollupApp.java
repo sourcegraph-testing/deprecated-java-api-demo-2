b19eb767e0ba4bfd882edfe8e1693744a1752abc
Travis Lyons <travis.lyons@sourcegraph.com>
2026-04-28 10:59:23 -0400

    Initial commit

---
+++ src/main/java/dev/trly/java/example/primitive/wrappers/OrderRollupApp.java
@@ -0,0 +1,33 @@
+package dev.trly.java.example.primitive.wrappers;
+
+public final class OrderRollupApp {
+    private OrderRollupApp() {
+    }
+
+    public static void main(String[] args) {
+        final Integer[] boxCounts = {
+            new Integer(4),
+            new Integer(7),
+            new Integer(3),
+            new Integer(5)
+        };
+        final Long[] orderValues = {
+            new Long(1299L),
+            new Long(2450L),
+            new Long(875L),
+            new Long(1625L)
+        };
+
+        int totalBoxes = 0;
+        long totalCents = 0L;
+        for (int index = 0; index < boxCounts.length; index++) {
+            final int boxes = boxCounts[index].intValue();
+            final long cents = orderValues[index].longValue();
+            totalBoxes += boxes;
+            totalCents += cents;
+            System.out.printf("Order %d: %d boxes, $%.2f%n", index + 1, boxes, cents / 100.0);
+        }
+
+        System.out.printf("Shipped %d boxes for $%.2f%n", totalBoxes, totalCents / 100.0);
+    }
+}
